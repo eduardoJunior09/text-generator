9608daaf10a0521db45a343ddae54709e242dee6
eduardoJunior09 <eduardojunior.dev@gmail.com>
2025-04-29 19:11:55 -0300

    feat: add novos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,19 @@ import { useState } from "react";
 import "./App.css";
 import Buttontext from "./components/Buttons/ButtonText";
 import TextArea from "./components/TextArea/TextArea";
+import Label from "./components/Label/Label";
+import InputSelect from "./components/Inputs/InputSelect";
+import InputText from "./components/Inputs/InputText";
+import Footer from "./components/Footer/Footer";
+
+const frutas = [
+  { label: "Maçã", value: "maca" },
+  { label: "Banana", value: "banana" },
+  { label: "Laranja", value: "laranja" },
+];
 
 const textAPI =
-  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, iure distinctio cupiditate ullam omnis dignissimos laudantium voluptatum recusandae sunt quas voluptas quibusdam delectus. Reiciendis delectus maiores nulla ab distinctio consectetur. ";
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, iure distinctio cupiditate ullam omnis dignissimos laudantium voluptatum recusandae sunt quas voluptas quibusdam delectus. Reiciendis delectus maiores nulla ab distinctio consectetur.";
 
 export default function App() {
   const [copySuccess, setCopySuccess] = useState(false);
@@ -20,7 +30,7 @@ export default function App() {
   };
 
   return (
-    <div className="flex items-start justify-center w-full h-screen bg-background-60 text-text p-2">
+    <div className="flex items-start justify-center w-full h-full bg-background-60 text-text p-2">
       <main className="flex flex-col items-center gap-8 w-full lg:max-w-3/6 p-2 lg:p-3 rounded-md">
         <section className="flex items-start justify-center flex-col gap-4 w-full">
           <div className="flex items-center justify-center gap-1 w-full text-3xl lg:text-4xl">
@@ -32,30 +42,53 @@ export default function App() {
             seu nível e preferências.
           </p>
         </section>
-        <section className="w-full p-2 lg:p-3 rounded-md bg-background-30">
-          <p>Inputs, Select e Botão</p>
+        <section className="flex  flex-col gap-5 w-full p-2 lg:p-3 rounded-md bg-background-30">
+          <div className=" flex items-center justify-between flex-col sm:flex-row gap-3">
+            <div className="flex flex-col gap-1 w-full sm:w-1/2 ">
+              <Label label="Categoria" />
+              <InputSelect name="frutas" options={frutas} />
+            </div>
+            <div className="flex flex-col gap-1 w-full sm:w-1/2 ">
+              <Label label="Idioma" />
+              <InputSelect name="frutas" options={frutas} />
+            </div>
+          </div>
+          <div className=" flex items-start justify-start flex-col gap-1">
+            <Label label="Gênero Textual" />
+            <p className="text-sm font-light">
+              Abaixo, informe o estilo de texto da sua preferência ou, se
+              preferir, palavras-chave. Exemplo: aprendizado de um novo idioma.
+            </p>
+            <InputText />
+          </div>
         </section>
-        
-        <section className="flex flex-col items-start justify-start w-full p-2 lg:p-3 rounded-md bg-background-30">
-          <div className="flex items-center justify-center sm:justify-end gap-3 w-full p-3">
-            <Buttontext
-              icon={<i className="bi bi-copy"></i>}
-              label={copySuccess ? "Copiado!" : "Copiar"}
-              onClick={() => copiarTexto(textAPI)}
-            />
 
-            <Buttontext
-              icon={<i className="bi bi-arrow-clockwise"></i>}
-              label="Redefinir"
-            />
+        <section className="flex flex-col items-start justify-start gap-2 w-full p-2 lg:p-3 rounded-md bg-background-30">
+          <div className="flex items-center justify-center sm:justify-between flex-col sm:flex-row gap-1 w-full p-3">
+            <div className="w-full text-center sm:text-start">
+              <p className="font-semibold text-xl">Texto Gerado</p>
+            </div>
+            <div className="flex items-center justify-center sm:justify-end gap-3 w-full p-3">
+              <Buttontext
+                icon={<i className="bi bi-copy"></i>}
+                label={copySuccess ? "Copiado!" : "Copiar"}
+                onClick={() => copiarTexto(textAPI)}
+              />
 
-            <Buttontext
-              icon={<i className="bi bi-cloud-arrow-down"></i>}
-              label="Baixar"
-            />
+              <Buttontext
+                icon={<i className="bi bi-arrow-clockwise"></i>}
+                label="Redefinir"
+              />
+
+              <Buttontext
+                icon={<i className="bi bi-cloud-arrow-down"></i>}
+                label="Baixar"
+              />
+            </div>
           </div>
           <TextArea text={textAPI} />
         </section>
+        <Footer />
       </main>
     </div>
   );
