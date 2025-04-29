818fdc5fa393aafe3a144afa17cd247eca409509
eduardoJunior09 <eduardojunior.dev@gmail.com>
2025-04-29 19:10:06 -0300

    Feat: componete footer criado, para o rodapé da ferramenta

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="w-full text-center">
+      <p className="text-text-destaque">
+        © 2025 LinguaGen. Todos os direitos reservados.
+      </p>
+    </footer>
+  );
+}
