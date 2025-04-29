78ca5851d92bf002c94fdf688366e102eb8f269d
eduardoJunior09 <eduardojunior.dev@gmail.com>
2025-04-29 19:10:21 -0300

    Style: definição de tamanho

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export default function TextArea({ text }: Props) {
   return (
-    <div className="w-full h-auto p-2 border-2 border-text-destaque rounded-md shadow-lg">
+    <div className="w-full h-auto max-h-96 overflow-auto  p-2 border-2 border-text-destaque rounded-md shadow-lg">
     {text}
   </div>
   );
