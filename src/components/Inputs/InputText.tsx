cd7dda74eacf267a5c9eb51103be477744a7ee88
eduardoJunior09 <eduardojunior.dev@gmail.com>
2025-04-29 19:11:22 -0300

    Feat: add componente input para descrição do gênero do texto de preferencia do usuário

---
+++ src/components/Inputs/InputText.tsx
@@ -0,0 +1,9 @@
+export default function InputText() {
+  return (
+    <input
+      type="text"
+      placeholder="Digite aqui..."
+      className="bg-background-10 border border-text text-text text-sm rounded-md focus:bg-background-10 focus:border-text-destaque block w-full px-1 py-2 outline-none transition duration-300 ease-in-out"
+    />
+  );
+}
